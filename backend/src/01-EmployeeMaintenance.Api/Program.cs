760622c6db8c06d7138a1e1f7142ed3d8b896795
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-07 17:06:47 -0300

    Add entity framework in project and configured context

--- backend/src/01-EmployeeMaintenance.Api/Program.cs
+++ backend/src/01-EmployeeMaintenance.Api/Program.cs
@@ -1,25 +1,36 @@
+using EmployeeMaintenance.Infra.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EmployeeMaintenanceContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        it => it.MigrationsAssembly(typeof(EmployeeMaintenanceContext).Assembly.FullName)
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<EmployeeMaintenanceContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
---
+++ backend/src/03-EmployeeMaintenance.Infra/Persistence/EmployeeMaintenanceContext.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeMaintenance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMaintenance.Infra.Persistence
+{
+    public class EmployeeMaintenanceContext : DbContext
+    {
+        public EmployeeMaintenanceContext(DbContextOptions<EmployeeMaintenanceContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region Primary Keys
+
+            modelBuilder.Entity<User>()
+              .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Address>()
+             .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Employee>()
+             .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Department>()
+             .HasKey(d => d.Id);
+
+            #endregion Primary Keys
+
+            #region Relationships
+
+            modelBuilder.Entity<Address>()
+                .HasOne(a => a.User)
+                .WithOne()
+                .HasForeignKey<Address>(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Department)
+                .WithMany()
+                .HasForeignKey(e => e.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.User)
+                .WithOne()
+                .HasForeignKey<Employee>(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            #endregion Relationships
+        }
+    }
+}
