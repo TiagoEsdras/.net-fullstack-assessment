fb56d0dbff58ef71f5bc93b567ef30131b6a5494
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 13:25:21 -0300

    Created test for ToEntity from CreateUserCommand

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Commands/Users/CreateUserCommandTests.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeMaintenance.Domain.Entities;
+using EmployeeMaintenance.Tests.Builders.Commands.Users;
+using FluentAssertions;
+
+namespace EmployeeMaintenance.Tests.Application.Commands.Users
+{
+    public class CreateUserCommandTests
+    {
+        [Fact]
+        public void ToEntity_ShouldReturnUser_WithCorrectProperties()
+        {
+            // Arrange
+            var addressCommand = new UserAddressCommandBuilder().Build();
+            var userCommand = new CreateUserCommandBuilder().WithAddress(addressCommand).Build();
+
+            // Act
+            var user = userCommand.ToEntity();
+
+            // Assert
+            user.Should().NotBeNull();
+            user.FirstName.Should().Be(userCommand.FirstName);
+            user.LastName.Should().Be(userCommand.LastName);
+            user.Phone.Should().Be(userCommand.Phone);
+            user.Address.Should().NotBeNull();
+            user.Should().BeOfType<User>();
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/Commands/Users/CreateUserCommandBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.Commands.Users;
+
+namespace EmployeeMaintenance.Tests.Builders.Commands.Users
+{
+    public class CreateUserCommandBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly CreateUserCommand _instance;
+
+        public CreateUserCommandBuilder()
+        {
+            _instance = new CreateUserCommand
+            {
+                FirstName = _faker.Name.FirstName(),
+                LastName = _faker.Name.LastName(),
+                Phone = _faker.Phone.PhoneNumber(),
+                Address = new UserAddressCommandBuilder().Build()
+            };
+        }
+
+        public CreateUserCommandBuilder WithFirstName(string firstName)
+        {
+            _instance.FirstName = firstName;
+            return this;
+        }
+
+        public CreateUserCommandBuilder WithLastName(string lastName)
+        {
+            _instance.LastName = lastName;
+            return this;
+        }
+
+        public CreateUserCommandBuilder WithPhone(string phone)
+        {
+            _instance.Phone = phone;
+            return this;
+        }
+
+        public CreateUserCommandBuilder WithAddress(UserAddressCommand address)
+        {
+            _instance.Address = address;
+            return this;
+        }
+
+        public CreateUserCommand Build() => _instance;
+    }
+}
