c0a2d14ca822c9acd56300122a6d4b10b315f914
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 14:45:13 -0300

    Created CreateEmployeeCommandTests

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Commands/Employees/CreateEmployeeCommandTests.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeMaintenance.Domain.Entities;
+using EmployeeMaintenance.Tests.Builders.Commands.Employees;
+using FluentAssertions;
+
+namespace EmployeeMaintenance.Tests.Application.Commands.Employees
+{
+    public class CreateEmployeeCommandTests
+    {
+        [Fact]
+        public void ToEntity_ShouldReturnEmployee_WithCorrectProperties()
+        {
+            // Arrange
+            var employeeCommand = new CreateEmployeeCommandBuilder().Build();
+
+            // Act
+            var employee = employeeCommand.ToEntity();
+
+            // Assert
+            employee.Should().NotBeNull();
+            employee.HireDate.Should().Be(employeeCommand.HireDate);
+            employee.UserId.Should().Be(employeeCommand.UserId);
+            employee.DepartmentId.Should().Be(employeeCommand.DepartmentId);
+            employee.Should().BeOfType<Employee>();
+        }
+    }
+}
