9fa79e5d5b04ab4dfa7f54540e03682dcb35a9ec
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 15:37:39 -0300

    Created AddressRequestDtoValidatorTests

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Validators/AddressRequestDtoValidatorTests.cs
@@ -0,0 +1,179 @@
+﻿using EmployeeMaintenance.Application.DTOs.Request;
+using EmployeeMaintenance.Application.Shared;
+using EmployeeMaintenance.Application.Validators.Addresses;
+using EmployeeMaintenance.Tests.Builders.DTOs.Request;
+using FluentAssertions;
+
+namespace EmployeeMaintenance.Tests.Application.Validators
+{
+    public class AddressRequestDtoValidatorTests
+    {
+        private readonly AddressRequestDtoValidator _validator;
+
+        public AddressRequestDtoValidatorTests()
+        {
+            _validator = new AddressRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Validator_ShouldPass_WhenAddressRequestIsValid()
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder().Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validator_ShouldFail_WhenStreetIsEmpty(string invalidStreet)
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithStreet(invalidStreet)
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.Street));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenStreetLengthIsInvalid()
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithStreet(new string('a', 256))
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.Street));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldMustHaveLengthBetweenCode);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validator_ShouldFail_WhenCityIsEmpty(string invalidCity)
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithCity(invalidCity)
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.City));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenCityLengthIsInvalid()
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithCity(new string('a', 101))
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.City));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldMustHaveLengthBetweenCode);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validator_ShouldFail_WhenStateIsEmpty(string invalidState)
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithState(invalidState)
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.State));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenStateLengthIsInvalid()
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithState(new string('a', 101))
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.State));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldMustHaveLengthBetweenCode);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validator_ShouldFail_WhenZipCodeIsEmpty(string invalidZipCode)
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithZipCode(invalidZipCode)
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.ZipCode));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenZipLengthIsInvalid()
+        {
+            // Arrange
+            var request = new AddressRequestDtoBuilder()
+                .WithZipCode(new string('a', 21))
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(AddressRequestDto.ZipCode));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldMustHaveLengthBetweenCode);
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/DTOs/Request/AddressRequestDtoBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.DTOs.Request;
+
+namespace EmployeeMaintenance.Tests.Builders.DTOs.Request
+{
+    public class AddressRequestDtoBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly AddressRequestDto _instance;
+
+        public AddressRequestDtoBuilder()
+        {
+            _instance = new AddressRequestDto
+            {
+                Street = _faker.Address.StreetAddress(),
+                City = _faker.Address.City(),
+                State = _faker.Address.State(),
+                ZipCode = _faker.Address.ZipCode()
+            };
+        }
+
+        public AddressRequestDtoBuilder WithStreet(string street)
+        {
+            _instance.Street = street;
+            return this;
+        }
+
+        public AddressRequestDtoBuilder WithCity(string city)
+        {
+            _instance.City = city;
+            return this;
+        }
+
+        public AddressRequestDtoBuilder WithState(string state)
+        {
+            _instance.State = state;
+            return this;
+        }
+
+        public AddressRequestDtoBuilder WithZipCode(string zipCode)
+        {
+            _instance.ZipCode = zipCode;
+            return this;
+        }
+
+        public AddressRequestDto Build() => _instance;
+    }
+}
