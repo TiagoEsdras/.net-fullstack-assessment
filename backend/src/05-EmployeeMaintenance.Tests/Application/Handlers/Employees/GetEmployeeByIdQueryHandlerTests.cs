2a0f3ae10b80f16047baae09c820325261752e6e
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 13:39:31 -0300

    Created tests for GetEmployeeByIdQueryHandler

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Handlers/Employees/GetEmployeeByIdQueryHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using EmployeeMaintenance.Application.DTOs.Response;
+using EmployeeMaintenance.Application.Handlers.Employees;
+using EmployeeMaintenance.Application.Interfaces.Repositories;
+using EmployeeMaintenance.Application.Queries.Employees;
+using EmployeeMaintenance.Application.Shared;
+using EmployeeMaintenance.Application.Shared.Enums;
+using EmployeeMaintenance.Domain.Entities;
+using EmployeeMaintenance.Tests.Builders.DTOs.Response;
+using FluentAssertions;
+using Moq;
+
+namespace EmployeeMaintenance.Tests.Application.Handlers.Employees
+{
+    public class GetEmployeeByIdQueryHandlerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockEmployeeRepository;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly GetEmployeeByIdQueryHandler _handler;
+
+        public GetEmployeeByIdQueryHandlerTests()
+        {
+            _mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            _mockMapper = new Mock<IMapper>();
+            _handler = new GetEmployeeByIdQueryHandler(_mockEmployeeRepository.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnSuccess_WhenEmployeeIsFound()
+        {
+            // Arrange
+            var query = new GetEmployeeByIdQuery(Guid.NewGuid());
+
+            var employeeDto = new EmployeeResponseDtoBuilder()
+                .WithId(query.Id)
+                .Build();
+
+            _mockEmployeeRepository.Setup(r => r.GetByIdAsync(query.Id))
+                .ReturnsAsync(new Employee());
+            _mockMapper.Setup(m => m.Map<EmployeeResponseDto>(It.IsAny<Employee>()))
+                .Returns(employeeDto);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Status.Should().Be(ResultResponseKind.Success);
+            result.Data.Should().BeEquivalentTo(employeeDto);
+            result.Message.Should().Be(string.Format(SuccessMessages.GetEntityByTermWithSuccess, nameof(Employee), nameof(query.Id)));
+            _mockEmployeeRepository.Verify(r => r.GetByIdAsync(query.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnNotFound_WhenEmployeeIsNotFound()
+        {
+            // Arrange
+            var query = new GetEmployeeByIdQuery(Guid.NewGuid());
+
+            _mockEmployeeRepository.Setup(r => r.GetByIdAsync(query.Id))
+                .ReturnsAsync((Employee)null!);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Status.Should().Be(ResultResponseKind.NotFound);
+            result.ErrorType.Should().Be(ErrorType.DataNotFound);
+            result.Message.Should().Be(string.Format(ErrorMessages.NotFoundEntity, nameof(Employee)));
+            result.Errors.Should().ContainEquivalentOf(new ErrorMessage(ErrorCodes.NotFoundEntityByTermCode, string.Format(ErrorMessages.NotFoundEntityByTerm, nameof(Employee), nameof(query.Id), query.Id)));
+            _mockEmployeeRepository.Verify(r => r.GetByIdAsync(query.Id), Times.Once);
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/DTOs/Response/EmployeeResponseDtoBuilder.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.DTOs.Response;
+
+namespace EmployeeMaintenance.Tests.Builders.DTOs.Response
+{
+    public class EmployeeResponseDtoBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly EmployeeResponseDto _instance;
+
+        public EmployeeResponseDtoBuilder()
+        {
+            _instance = new EmployeeResponseDto
+            {
+                Id = Guid.NewGuid(),
+                HireDate = _faker.Date.Past(),
+                Department = new DepartmentResponseDtoBuilder().Build(),
+                DepartmentId = Guid.NewGuid(),
+                User = new UserResponseDtoBuilder().Build(),
+                UserId = Guid.NewGuid()
+            };
+        }
+
+        public EmployeeResponseDtoBuilder WithId(Guid id)
+        {
+            _instance.Id = id;
+            return this;
+        }
+
+        public EmployeeResponseDtoBuilder WithHireDate(DateTime hireDate)
+        {
+            _instance.HireDate = hireDate;
+            return this;
+        }
+
+        public EmployeeResponseDtoBuilder WithDepartment(DepartmentResponseDto department)
+        {
+            _instance.Department = department;
+            _instance.DepartmentId = department.Id;
+            return this;
+        }
+
+        public EmployeeResponseDtoBuilder WithUser(UserResponseDto user)
+        {
+            _instance.User = user;
+            _instance.UserId = user.Id;
+            return this;
+        }
+
+        public EmployeeResponseDto Build() => _instance;
+    }
+}
