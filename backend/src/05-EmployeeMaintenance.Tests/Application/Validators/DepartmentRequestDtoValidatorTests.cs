f85ae9abc6add1d9de8df1078f5c1a8dd3675e06
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 16:12:05 -0300

    Created DepartmentRequestDtoValidatorTests

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Validators/DepartmentRequestDtoValidatorTests.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeMaintenance.Application.DTOs.Request;
+using EmployeeMaintenance.Application.Shared;
+using EmployeeMaintenance.Application.Validators.Departments;
+using EmployeeMaintenance.Tests.Builders.DTOs.Request;
+using FluentAssertions;
+
+namespace EmployeeMaintenance.Tests.Application.Validators.Departments
+{
+    public class DepartmentRequestDtoValidatorTests
+    {
+        private readonly DepartmentRequestDtoValidator _validator;
+
+        public DepartmentRequestDtoValidatorTests()
+        {
+            _validator = new DepartmentRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Validator_ShouldPass_WhenDepartmentRequestIsValid()
+        {
+            // Arrange
+            var request = new DepartmentRequestDtoBuilder().Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validator_ShouldFail_WhenDepartmentNameIsEmpty(string invalidDepartmentName)
+        {
+            // Arrange
+            var request = new DepartmentRequestDtoBuilder().WithDepartmentName(invalidDepartmentName).Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(DepartmentRequestDto.DepartmentName));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenDepartmentNameLengthIsInvalid()
+        {
+            // Arrange
+            var request = new DepartmentRequestDtoBuilder()
+                .WithDepartmentName(new string('a', 101))
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(DepartmentRequestDto.DepartmentName));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldMustHaveLengthBetweenCode);
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/DTOs/Request/DepartmentRequestDtoBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.DTOs.Request;
+
+namespace EmployeeMaintenance.Tests.Builders.DTOs.Request
+{
+    public class DepartmentRequestDtoBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly DepartmentRequestDto _instance;
+
+        public DepartmentRequestDtoBuilder()
+        {
+            _instance = new DepartmentRequestDto
+            {
+                DepartmentName = _faker.Commerce.Department()
+            };
+        }
+
+        public DepartmentRequestDtoBuilder WithDepartmentName(string departmentName)
+        {
+            _instance.DepartmentName = departmentName;
+            return this;
+        }
+
+        public DepartmentRequestDto Build() => _instance;
+    }
+}
