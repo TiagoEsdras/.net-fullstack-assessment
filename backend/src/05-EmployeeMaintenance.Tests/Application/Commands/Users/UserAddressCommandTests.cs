b3eeaeb3489514d95137929f53f8552359c97b05
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 13:19:18 -0300

    Created test for ToEntity from UserAddressCommand

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Commands/Users/UserAddressCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeMaintenance.Domain.Entities;
+using EmployeeMaintenance.Tests.Builders.Commands.Users;
+using FluentAssertions;
+
+namespace EmployeeMaintenance.Tests.Application.Commands.Users
+{
+    public class UserAddressCommandTests
+    {
+        [Fact]
+        public void ToEntity_ShouldReturnAddress_WithCorrectProperties()
+        {
+            // Arrange
+            var addressCommand = new UserAddressCommandBuilder().Build();
+
+            // Act
+            var address = addressCommand.ToEntity();
+
+            // Assert
+            address.Should().NotBeNull();
+            address.Street.Should().Be(addressCommand.Street);
+            address.City.Should().Be(addressCommand.City);
+            address.State.Should().Be(addressCommand.State);
+            address.ZipCode.Should().Be(addressCommand.ZipCode);
+            address.Should().BeOfType<Address>();
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/Commands/Users/UserAddressCommandBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.Commands.Users;
+
+namespace EmployeeMaintenance.Tests.Builders.Commands.Users
+{
+    public class UserAddressCommandBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly UserAddressCommand _instance;
+
+        public UserAddressCommandBuilder()
+        {
+            _instance = new UserAddressCommand
+            {
+                Street = _faker.Address.StreetAddress(),
+                City = _faker.Address.City(),
+                State = _faker.Address.State(),
+                ZipCode = _faker.Address.ZipCode()
+            };
+        }
+
+        public UserAddressCommandBuilder WithStreet(string street)
+        {
+            _instance.Street = street;
+            return this;
+        }
+
+        public UserAddressCommandBuilder WithCity(string city)
+        {
+            _instance.City = city;
+            return this;
+        }
+
+        public UserAddressCommandBuilder WithState(string state)
+        {
+            _instance.State = state;
+            return this;
+        }
+
+        public UserAddressCommandBuilder WithZipCode(string zipCode)
+        {
+            _instance.ZipCode = zipCode;
+            return this;
+        }
+
+        public UserAddressCommand Build() => _instance;
+    }
+}
