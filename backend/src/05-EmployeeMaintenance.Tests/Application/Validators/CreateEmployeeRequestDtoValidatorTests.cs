dedfbbd27f264891087e4591cf5ecba5ab3730bc
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-09 16:38:23 -0300

    Created CreateEmployeeRequestDtoValidatorTests

---
+++ backend/src/05-EmployeeMaintenance.Tests/Application/Validators/CreateEmployeeRequestDtoValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeMaintenance.Application.DTOs.Request;
+using EmployeeMaintenance.Application.Shared;
+using EmployeeMaintenance.Application.Validators.Employees;
+using EmployeeMaintenance.Tests.Builders.DTOs.Response;
+using FluentAssertions;
+using FluentValidation;
+using Moq;
+
+namespace EmployeeMaintenance.Tests.Application.Validators.Employees
+{
+    public class CreateEmployeeRequestDtoValidatorTests
+    {
+        private readonly CreateEmployeeRequestDtoValidator _validator;
+        private readonly Mock<IValidator<DepartmentRequestDto>> _mockDepartmentValidator;
+        private readonly Mock<IValidator<UserRequestDto>> _mockUserValidator;
+
+        public CreateEmployeeRequestDtoValidatorTests()
+        {
+            _mockDepartmentValidator = new Mock<IValidator<DepartmentRequestDto>>();
+            _mockUserValidator = new Mock<IValidator<UserRequestDto>>();
+            _validator = new CreateEmployeeRequestDtoValidator(_mockDepartmentValidator.Object, _mockUserValidator.Object);
+        }
+
+        [Fact]
+        public void Validator_ShouldPass_WhenEmployeeRequestIsValid()
+        {
+            // Arrange
+            var request = new EmployeeRequestDtoBuilder().Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validator_ShouldFail_WhenHireDateIsEmpty()
+        {
+            // Arrange
+            var request = new EmployeeRequestDtoBuilder()
+                .WithHireDate(default)
+                .Build();
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(EmployeeRequestDto.HireDate));
+            result.Errors.Should().Contain(e => e.ErrorCode == ErrorCodes.FieldCannotBeNullOrEmptyCode);
+        }
+    }
+}
---
+++ backend/src/05-EmployeeMaintenance.Tests/Builders/DTOs/Request/EmployeeRequestDtoBuilder.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using EmployeeMaintenance.Application.DTOs.Request;
+using EmployeeMaintenance.Tests.Builders.DTOs.Request;
+
+namespace EmployeeMaintenance.Tests.Builders.DTOs.Response
+{
+    public class EmployeeRequestDtoBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly EmployeeRequestDto _instance;
+
+        public EmployeeRequestDtoBuilder()
+        {
+            _instance = new EmployeeRequestDto
+            {
+                HireDate = _faker.Date.Past(),
+                Department = new DepartmentRequestDtoBuilder().Build(),
+                User = new UserRequestDtoBuilder().Build(),
+            };
+        }
+
+        public EmployeeRequestDtoBuilder WithHireDate(DateTime hireDate)
+        {
+            _instance.HireDate = hireDate;
+            return this;
+        }
+
+        public EmployeeRequestDtoBuilder WithDepartment(DepartmentRequestDto department)
+        {
+            _instance.Department = department;
+            return this;
+        }
+
+        public EmployeeRequestDtoBuilder WithUser(UserRequestDto user)
+        {
+            _instance.User = user;
+            return this;
+        }
+
+        public EmployeeRequestDto Build() => _instance;
+    }
+}
